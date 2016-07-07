c49b027a8abca5991adcb3d4faf9d925ef73c955
Natasha Singh <dukedesi22@gmail.com>
2016-07-06 22:06:14 -0400

    Changed comments

--- libs/js/natasha.js
+++ libs/js/natasha.js
@@ -1,5 +1,5 @@
 /*!
- * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Modified Start Bootstrap - Agency Bootstrap Theme 
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
